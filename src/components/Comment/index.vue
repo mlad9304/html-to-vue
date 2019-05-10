b35da54cfa3b4eecff6a39cdd6a29127598c7fa4
rndev80 <rndev80@gmail.com>
2019-05-11 01:04:06 +0300

    Added comment component

---
+++ src/components/Comment/index.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="comments__wrapper">
+    <div class="comments__item d-flex align-items-center" :class="{comments__item_mb: !mb}">
+      <img :src="require(`@/assets/img/${comment.profileImg}`)" alt="profile-image" class="comments__icon">
+      <div class="comments__data">
+        <div class="comments__info d-flex align-items-center">
+          <h5 class="comments__name">
+            {{ comment.name }}
+          </h5>
+          <p class="comments__time">
+            {{ comment.date }}
+          </p>
+        </div>
+        <p class="comments__text">
+          {{ comment.content }}
+        </p>
+      </div>
+    </div>
+    <img v-if="comment.contentImg" :src="require(`@/assets/img/${comment.contentImg}`)" alt="" class="tom-pic">
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'comment',
+  props: ['comment'],
+  computed: {
+    mb () {
+      return !this.comment.contentImg
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
--- src/views/congrats/feedback/components/Comments.vue
+++ src/views/congrats/feedback/components/Comments.vue
@@ -1,94 +1,58 @@
 <template>
   <div class="comments">
-    <div class="comments__item d-flex align-items-center">
-      <div class="comments__icon comments__icon_nicolas"></div>
-      <div class="comments__data">
-        <div class="comments__info d-flex align-items-center">
-          <h5 class="comments__name">
-            Nicolas Issa
-          </h5>
-          <p class="comments__time">
-            Avril 30, 2019 à 17:20
-          </p>
-        </div>
-        <p class="comments__text">
-          J'étais un peu sceptique au début, mais je viens de recevoir ma Samsung S9 aujourd'hui! c'etait facile et fun
-        </p>
-      </div>
-    </div>
-    <div class="comments__item d-flex align-items-center">
-      <div class="comments__icon comments__icon_martin"></div>
-      <div class="comments__data">
-        <div class="comments__info d-flex align-items-center">
-          <h5 class="comments__name">
-            Martin Dupont
-          </h5>
-          <p class="comments__time">
-            Avril 30, 2019 à 17:20
-          </p>
-        </div>
-        <p class="comments__text">
-          J'étais un peu sceptique au début, mais je viens de recevoir ma Samsung S9 aujourd'hui! c'etait facile et fun
-        </p>
-      </div>
-    </div>
-    <div class="comments__wrapper">
-      <div class="comments__item comments__item_mb d-flex align-items-center">
-        <div class="comments__icon comments__icon_tom"></div>
-        <div class="comments__data">
-          <div class="comments__info d-flex align-items-center">
-            <h5 class="comments__name">
-              Tom Nemo
-            </h5>
-            <p class="comments__time">
-              Avril 30, 2019 à 17:20
-            </p>
-          </div>
-          <p class="comments__text">
-            J'étais un peu sceptique au début, mais je viens de recevoir ma Samsung S9 aujourd'hui! c'etait facile et fun
-          </p>
-        </div>
-      </div>
-      <img src="@/assets/img/hands.jpg" alt="" class="tom-pic">
-    </div>
-    <div class="comments__item d-flex align-items-center">
-      <div class="comments__icon comments__icon_natalia"></div>
-      <div class="comments__data">
-        <div class="comments__info d-flex align-items-center">
-          <h5 class="comments__name">
-            Natalia Jamka
-          </h5>
-          <p class="comments__time">
-            Avril 30, 2019 à 17:20
-          </p>
-        </div>
-        <p class="comments__text">
-          J'étais un peu sceptique au début, mais je viens de recevoir ma Samsung S9 aujourd'hui! c'etait facile et fun
-        </p>
-      </div>
-    </div>
-    <div class="comments__item d-flex align-items-center">
-      <div class="comments__icon comments__icon_rick"></div>
-      <div class="comments__data">
-        <div class="comments__info d-flex align-items-center">
-          <h5 class="comments__name">
-            Rick Shaper
-          </h5>
-          <p class="comments__time">
-            Avril 30, 2019 à 17:20
-          </p>
-        </div>
-        <p class="comments__text">
-          J'étais un peu sceptique au début, mais je viens de recevoir ma Samsung S9 aujourd'hui! c'etait facile et fun
-        </p>
-      </div>
-    </div>
+    <Comment
+      v-for="(comment, index) in comments"
+      :key="index"
+      :comment="comment"
+    />
   </div>
 </template>
 
 <script>
+import Comment from '@/components/Comment'
+
 export default {
-  name: 'comments'
+  name: 'comments',
+  data () {
+    return {
+      comments: [
+        {
+          profileImg: 'nicolas.jpg',
+          name: 'Nicolas Issa',
+          date: 'Avril 30, 2019 à 17:20',
+          content: "J'étais un peu sceptique au début, mais je viens de recevoir ma Samsung S9 aujourd'hui! c'etait facile et fun"
+        },
+        {
+          profileImg: 'martin.jpg',
+          name: 'Martin Dupont',
+          date: 'Avril 30, 2019 à 17:20',
+          content: "J'étais un peu sceptique au début, mais je viens de recevoir ma Samsung S9 aujourd'hui! c'etait facile et fun"
+        },
+        {
+          profileImg: 'tom.jpg',
+          name: 'Tom Nemo',
+          date: 'Avril 30, 2019 à 17:20',
+          content: "J'étais un peu sceptique au début, mais je viens de recevoir ma Samsung S9 aujourd'hui! c'etait facile et fun",
+          contentImg: 'hands.jpg'
+        },
+        {
+          profileImg: 'natalia.jpg',
+          name: 'Natalia Jamka',
+          date: 'Avril 30, 2019 à 17:20',
+          content: "J'étais un peu sceptique au début, mais je viens de recevoir ma Samsung S9 aujourd'hui! c'etait facile et fun"
+        },
+        {
+          profileImg: 'rick.jpg',
+          name: 'Rick Shaper',
+          date: 'Avril 30, 2019 à 17:20',
+          content: "J'étais un peu sceptique au début, mais je viens de recevoir ma Samsung S9 aujourd'hui! c'etait facile et fun"
+        }
+      ]
+    }
+  },
+  components: {
+    Comment
+  }
 }
 </script>
 
